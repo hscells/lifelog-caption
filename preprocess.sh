7f73bafb425282257a14e98f4abf99b1d06be62b
Harry Scells <harryscells@gmail.com>
2016-09-01 10:55:40 +1000

    added images root to preprocess

--- preprocess.sh
+++ preprocess.sh
@@ -3,4 +3,4 @@
 #############################################################################
 
 python get-training-data.py $1 $2 &&
-python neuraltalk2-master/prepro.py --input_json $2 --num_val $3 --num_test $4
+python neuraltalk2-master/prepro.py --input_json $2 --num_val $3 --num_test $4 --images_root images
