52c65d10a1378d314cbaad9850c0eb3b8c6199b9
Harry Scells <harryscells@gmail.com>
2016-09-01 10:44:19 +1000

    simplified pipeline

--- run.sh
+++ run.sh
@@ -3,20 +3,20 @@
 #####################################################
 
 # First need to preprocess the data:
-# Arguments: $1=/path/to/dataset.xml $2=/path/to/images $3=number of images to assign to validation data
+# Arguments: $1=/path/to/dataset.xml $2=number of images to assign to validation data
 echo Preprocessing...
-./preprocess.sh $1 $2 output.json $3
+./preprocess.sh $1 output.json $2
 
 # Next need to train
 echo Training...
 d=$(pwd)
 cd $LIFELOG_CLASSIFICATION_DIR
 # This assumes that the neuraltalk2 dir is inside the caption dir
-th train.lua -input_h5 ../data.h5 -input_json ../data.json -gpuid -1 max_iters 100
+th train.lua -input_h5 ../data.h5 -input_json ../data.json -gpuid -1 max_iters 10000
 
 # Whoop, now we can evaluate!
 echo Evaluating...
-th eval.lua -model model_id.json -image_folder $2 -num_images $3 -gpuid -1
+th eval.lua -model model_id.json -image_folder ../images -num_images -1 -gpuid -1 -dump_json 1
 
 echo Done!
 echo You can see the results by running:
