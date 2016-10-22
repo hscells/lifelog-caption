bb44f6abf5ea150f60c6bbd4e7ce1e4d24ac4c15
Harry Scells <harryscells@gmail.com>
2016-10-22 21:07:16 +1000

    get the training data for other annotation types

--- get-training-data.py
+++ get-training-data.py
@@ -1,8 +1,14 @@
-import pg8000, sys, os, argparse, json, re
 from xml.dom import minidom
 
+import argparse
+import json
+import os
+import pg8000
+import re
+import sys
 
-def innerText(element, name):
+
+def inner_text(element, name):
     return element.getElementsByTagName(name)[0].firstChild.nodeValue
 
 
@@ -12,9 +18,9 @@ def export_json(dataset, output):
     images = xmldoc.getElementsByTagName('image')
     lookup = {}
     for image in images:
-        path = innerText(image, 'image-path')
-        id = path.split('/')[-1].split('.')[0]
-        lookup[id] = path
+        path = inner_text(image, 'image-path')
+        image_id = path.split('/')[-1].split('.')[0]
+        lookup[image_id] = path
 
     print("Connecting...")
     conn = pg8000.connect(user=os.environ.get("LIFELOG_DB_USER"),
@@ -22,23 +28,34 @@ def export_json(dataset, output):
                           host=os.environ.get("LIFELOG_DB_HOST"),
                           database=os.environ.get("LIFELOG_DB_NAME"))
     cursor = conn.cursor()
-    sql = 'SELECT i.name, a.annotation from annotated_text_images a JOIN images i ON a.image_id = i.id;'
-    cursor.execute(sql)
+    tables = ['annotated_text_images', 'annotated_query_images', 'annotated_tag_images']
 
-    results = cursor.fetchall()
-    data = []
-    for row in results:
-        id, caption = row
-        caption = re.sub(r'(.)\.(.)', r'\1. \2', caption)
-        if len(caption.strip()) > 0:
-            captions = caption.split('.')
-            image_name = lookup[id].split("/")[-1]
-            data.append({'file_path': os.getcwd() + '/neuraltalk2-master/images/' + image_name,
-                         'captions': captions, 'id': image_name.split('.')[0]})
+    for table in tables:
+        sql = 'SELECT i.name, a.annotation ' \
+              'FROM {} a ' \
+              'JOIN images i ON a.image_id = i.id;'.format(table)
+        cursor.execute(sql)
+
+        results = cursor.fetchall()
+        data = []
+        for row in results:
+            image_id, caption = row
+            if table in ['annotated_text_images', 'annotated_query_images']:
+                caption = str(re.sub(r'(.)\.(.)', r'\1. \2', caption)).strip()
+                if len(caption) > 0:
+                    captions = caption.split('.')
+                    captions = list(
+                        filter(None, [x.replace(',', '').strip() for x in captions]))
+            else:
+                captions = caption
+            if len(captions) > 0:
+                image_name = lookup[image_id].split("/")[-1]
+                data.append({'file_path': '/neuraltalk2/flat-images/' + image_name,
+                             'captions': captions, 'id': image_name.split('.')[0]})
+
+        with open('{}-{}'.format(table, output), 'w') as f:
+            f.write(json.dumps(data, sort_keys=False, indent=2, separators=(',', ': ')))
     cursor.close()
-
-    with open(output, 'w') as f:
-        f.write(json.dumps(data, sort_keys=False, indent=2, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
